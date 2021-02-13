d7d272723da6caeeb4a73d1dd4df6bb1592590fa
Carlos Alexandro Becker <caarlos0@gmail.com>
2021-02-13 15:46:33 -0300

    fix: handle passing absolute paths
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>

--- testfs.go
+++ testfs.go
@@ -5,9 +5,11 @@
 package testfs
 
 import (
+	"fmt"
 	"io/fs"
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 )
 
@@ -34,12 +36,26 @@ func (t FS) Path() string {
 	return t.path
 }
 
+var ErrOutsideFS = fmt.Errorf("path is outside test fs root folder")
+
 // WriteFile writes a file to FS.
 func (t FS) WriteFile(name string, data []byte, perm os.FileMode) error {
+	if filepath.IsAbs(name) {
+		if strings.HasPrefix(name, t.path) {
+			return os.WriteFile(name, data, perm)
+		}
+		return fmt.Errorf("%s: %w", name, ErrOutsideFS)
+	}
 	return os.WriteFile(filepath.Join(t.path, name), data, perm)
 }
 
 // MkdirAll creates the dir and all the necessary parents into FS.
 func (t FS) MkdirAll(path string, perm os.FileMode) error {
+	if filepath.IsAbs(path) {
+		if strings.HasPrefix(path, t.path) {
+			return os.MkdirAll(path, perm)
+		}
+		return fmt.Errorf("%s: %w", path, ErrOutsideFS)
+	}
 	return os.MkdirAll(filepath.Join(t.path, path), perm)
 }
--- testfs_test.go
+++ testfs_test.go
@@ -26,12 +26,29 @@ func TestFS(t *testing.T) {
 		t.Fatalf("expected Path to be %s, got %s", tmpfs.path, tmpfs.Path())
 	}
 
+	content := "example"
 	testfile := "foo/bar/foobar"
+	testfile2 := "asd/sada/aaaa"
+
+	if err := tmpfs.MkdirAll("/tmp/asd", 0o700); err == nil {
+		t.Fatalf("expected to fail")
+	}
+
+	if err := tmpfs.WriteFile("/tmp/asd", []byte(content), 0o700); err == nil {
+		t.Fatalf("expected to fail")
+	}
 
 	if err := tmpfs.MkdirAll(filepath.Dir(testfile), 0o764); err != nil {
 		t.Fatalf("failed to create dir: %s", err)
 	}
-	if err := tmpfs.WriteFile(testfile, []byte("example"), 0o644); err != nil {
+	if err := tmpfs.WriteFile(testfile, []byte(content), 0o644); err != nil {
+		t.Fatalf("failed to create dir: %s", err)
+	}
+
+	if err := tmpfs.MkdirAll(filepath.Dir(filepath.Join(tmpfs.Path(), testfile2)), 0o764); err != nil {
+		t.Fatalf("failed to create dir: %s", err)
+	}
+	if err := tmpfs.WriteFile(filepath.Join(tmpfs.Path(), testfile2), []byte(content), 0o644); err != nil {
 		t.Fatalf("failed to create dir: %s", err)
 	}
 
@@ -39,11 +56,19 @@ func TestFS(t *testing.T) {
 	if err != nil {
 		t.Fatalf("failed to read %s: %s", testfile, err)
 	}
-	if string(bts) != "example" {
-		t.Fatalf("invalid %s contents, got %s, want %s", testfile, string(bts), "example")
+	if string(bts) != content {
+		t.Fatalf("invalid %s contents, got %s, want %s", testfile, string(bts), content)
 	}
 
-	if err := fstest.TestFS(tmpfs, "foo/bar/foobar"); err != nil {
+	bts, err = fs.ReadFile(tmpfs, testfile2)
+	if err != nil {
+		t.Fatalf("failed to read %s: %s", testfile2, err)
+	}
+	if string(bts) != content {
+		t.Fatalf("invalid %s contents, got %s, want %s", testfile2, string(bts), content)
+	}
+
+	if err := fstest.TestFS(tmpfs, testfile, testfile2); err != nil {
 		t.Fatalf("failed to check fs: %s", err)
 	}
 }
